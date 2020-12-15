2521478d2cfc460ced51d9804946553231ecb22a
Rolan C. Daniel <rolancdaniel@gmail.com>
2020-12-15 22:48:58 +0800

    Favorites

--- src/layout/Contacts/Favorites/index.js
+++ src/layout/Contacts/Favorites/index.js
@@ -1,16 +1,55 @@
-import React from 'react'
-import { Placeholder } from 'semantic-ui-react'
+import React,{useRef} from 'react'
+import { Icon, Placeholder } from 'semantic-ui-react'
 import ImageThumb from '../../../components/ImageThumb'
+import './style.css'
 
 function Favorites({ favorites, loading }) {
     console.log('Favorites', favorites)
+    const listRef = useRef(null);
+
+    const scrollLeft = () =>{
+
+        if(listRef.current){
+            listRef.current.scrollBy({
+                top:0,
+                left:200,
+                behavior: 'smooth',    
+            });
+        }
+
+    }
+
+    const scrollRight = () =>{
+        if(listRef.current){
+            listRef.current.scrollBy({
+                top:0,
+                left: -200,
+                behavior: 'smooth',    
+            });
+        }
+
+    }
     return (
-        <div>
+        <div className='slide-container'>
+            <Icon name='caret left' size='large' onClick={scrollRight}></Icon>
             {favorites.length > 0 && (
-                <div className='items-container'>
+                <div className='items-container' ref={listRef}>
                     {Array.isArray(favorites) &&
                         favorites.map((item) => {
-                            console.log('Favorites item: ', item)
+                            return (
+                                <div key={item.id} className='single-item-container'>
+                                    <ImageThumb
+                                        firstName={item.first_name}
+                                        lastName={item.last_name}
+                                        src={item.contact_picture}
+                                        style={{ width: 75, height: 75 }}
+                                    />
+                                    <p className='name'>
+                                        {item.first_name}
+                                        {item.last_name}
+                                    </p>
+                                </div>
+                            )
                         })}
                 </div>
             )}
@@ -26,6 +65,8 @@ function Favorites({ favorites, loading }) {
                     </Placeholder>
                 </>
             )}
+
+            <Icon name='caret right' size='large' onClick={scrollLeft}></Icon>
         </div>
     )
 }
